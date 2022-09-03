aa98ba68d630a085e119b89574c3c95435a8b11c
Imran Ahmad <imran.ahmad@navis.com>
2022-09-02 21:12:40 -0700

    Added the edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,14 +9,50 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+  def new
+    #debugger
+    @article = Article.new
   end
 
-  def new
+  def edit
+    @article = Article.find(params[:id])
+  end
 
+  def update
+    @article = Article.find(params[:id])
+    respond_to do |format|
+      if @article.update(params.require(:article).permit(:title, :description))
+        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
+        # format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        # format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+
+
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    respond_to do |format|
+      if @article.save
+        flash[:notice] = "Article saved successfully"
+        format.html { redirect_to article_url(@article) }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+
+    # following did not work for error case!!!
+    # if @article.save
+    #   redirect_to @article
+    # else
+    #   render 'new'
+    # end
   end
 
 
