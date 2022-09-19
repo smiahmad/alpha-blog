bee08468d19c658f18c9d41c2740e2c9194d33a2
Imran Ahmad <imran.ahmad@navis.com>
2022-09-18 22:45:19 -0700

    added association to user backend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,6 +32,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
 
     respond_to do |format|
       if @article.save
