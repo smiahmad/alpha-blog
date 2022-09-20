3da56106d0573ffa83e09ff3d12f910981ccae68
Imran Ahmad <imran.ahmad@navis.com>
2022-09-19 17:56:15 -0700

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        flash[:notice] = "Welcome to Alpha Blog #{@user.username}. You have successfully signed up!"
+        format.html { redirect_to article_url(@user) }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
