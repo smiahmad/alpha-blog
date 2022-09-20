6e3c3c26a616a0bb67e68eb3958574f69838aa2a
Imran Ahmad <imran.ahmad@navis.com>
2022-09-20 16:35:06 -0700

    Add users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravitar_for(user, options = {size: 80})
+    email_adderss = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_adderss)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{options[:size]}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
 end
