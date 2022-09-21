b94da9b1f31bddf5005b312e416313b011494419
Imran Ahmad <imran.ahmad@navis.com>
2022-09-20 17:06:15 -0700

    Added users index etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def index
+    #debugger
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
